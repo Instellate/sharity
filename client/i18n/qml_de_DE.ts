5cf26b7d69227ec6d95fd837e655dc47d41b7f7c
yuki6942 <56565042+yuki6942@users.noreply.github.com>
2025-08-24 16:34:36 +0200

    Added German Translation

---
+++ client/i18n/qml_de_DE.ts
@@ -0,0 +1,199 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE" sourcelanguage="en_GB">
+<context>
+    <name>Connect</name>
+    <message>
+        <location filename="../qml/Connect.qml" line="24"/>
+        <source>WebSocket Relay</source>
+        <translation>WebSocket Relay</translation>
+    </message>
+    <message>
+        <location filename="../qml/Connect.qml" line="35"/>
+        <source>Type</source>
+        <translation>Typ</translation>
+    </message>
+    <message>
+        <location filename="../qml/Connect.qml" line="40"/>
+        <source>Uploader</source>
+        <translation>Uploader</translation>
+    </message>
+    <message>
+        <location filename="../qml/Connect.qml" line="45"/>
+        <source>Downloader</source>
+        <translation>Downloader</translation>
+    </message>
+    <message>
+        <location filename="../qml/Connect.qml" line="68"/>
+        <source>Copy Connection Key</source>
+        <translation>Verbindungsschlüssel kopieren</translation>
+    </message>
+    <message>
+        <location filename="../qml/Connect.qml" line="73"/>
+        <source>Copied to clipboard</source>
+        <translation>In Zwischenablage kopiert</translation>
+    </message>
+    <message>
+        <location filename="../qml/Connect.qml" line="84"/>
+        <source>Connection Key</source>
+        <translation>Verbindungsschlüssel</translation>
+    </message>
+    <message>
+        <location filename="../qml/Connect.qml" line="90"/>
+        <source>Connect</source>
+        <translation>Verbinden</translation>
+    </message>
+</context>
+<context>
+    <name>Downloader</name>
+    <message>
+        <location filename="../qml/Downloader.qml" line="13"/>
+        <source>Downloader</source>
+        <translation>Downloader</translation>
+    </message>
+    <message>
+        <location filename="../qml/Downloader.qml" line="21"/>
+        <source>Waiting</source>
+        <translation>Warten</translation>
+    </message>
+    <message>
+        <location filename="../qml/Downloader.qml" line="23"/>
+        <source>Downloading</source>
+        <translation>Wird heruntergeladen</translation>
+    </message>
+    <message>
+        <location filename="../qml/Downloader.qml" line="25"/>
+        <source>Downloaded</source>
+        <translation>Wurde heruntergeladen</translation>
+    </message>
+    <message>
+        <location filename="../qml/Downloader.qml" line="34"/>
+        <source>%1  out of %2</source>
+        <extracomment>How much of the file has been downloaded out of the total amount</extracomment>
+        <translation>%1 von %2</translation>
+    </message>
+    <message>
+        <location filename="../qml/Downloader.qml" line="53"/>
+        <source>Cancel</source>
+        <translation>Abbrechen</translation>
+    </message>
+</context>
+<context>
+    <name>HandshakeStatus</name>
+    <message>
+        <location filename="../qml/HandshakeStatus.qml" line="11"/>
+        <source>Waiting to be etablished</source>
+        <translation>Verbindung wird aufgebaut</translation>
+    </message>
+    <message>
+        <location filename="../qml/HandshakeStatus.qml" line="13"/>
+        <source>Waiting for handshake to finish</source>
+        <translation>Warte auf Abschluss des Handshakes</translation>
+    </message>
+    <message>
+        <location filename="../qml/HandshakeStatus.qml" line="16"/>
+        <source>Established and encrypted</source>
+        <translation>Verbindung hergestellt und verschlüsselt</translation>
+    </message>
+    <message>
+        <location filename="../qml/HandshakeStatus.qml" line="23"/>
+        <source>Cancel</source>
+        <translation>Abbrechen</translation>
+    </message>
+</context>
+<context>
+    <name>Sas</name>
+    <message>
+        <location filename="../qml/Sas.qml" line="24"/>
+        <source>Waiting to establish SAS</source>
+        <translation>SAS wird aufgebaut</translation>
+    </message>
+    <message>
+        <location filename="../qml/Sas.qml" line="26"/>
+        <source>Verify with the other that the values are equal</source>
+        <translation>Überprüfe mit dem anderen, ob die Werte gleich sind</translation>
+    </message>
+    <message>
+        <location filename="../qml/Sas.qml" line="28"/>
+        <source>Waiting on the other the confirm</source>
+        <translation>Warte auf Bestätigung des anderen</translation>
+    </message>
+    <message>
+        <location filename="../qml/Sas.qml" line="31"/>
+        <source>Both confirmed equal sas</source>
+        <translation>Beide haben gleichen sas bestätigt</translation>
+    </message>
+    <message>
+        <location filename="../qml/Sas.qml" line="39"/>
+        <source>Numbers</source>
+        <translation>Zahlen</translation>
+    </message>
+    <message>
+        <location filename="../qml/Sas.qml" line="39"/>
+        <source>Emojis</source>
+        <translation>Emojis</translation>
+    </message>
+    <message>
+        <location filename="../qml/Sas.qml" line="59"/>
+        <source>Are they equal?</source>
+        <translation>Sind sie gleich?</translation>
+    </message>
+    <message>
+        <location filename="../qml/Sas.qml" line="69"/>
+        <source>Yes</source>
+        <translation>Ja</translation>
+    </message>
+    <message>
+        <location filename="../qml/Sas.qml" line="74"/>
+        <source>No</source>
+        <translation>Nein</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPopup</name>
+    <message>
+        <location filename="../qml/SettingsPopup.qml" line="52"/>
+        <source>Language</source>
+        <translation>Sprache</translation>
+    </message>
+</context>
+<context>
+    <name>Uploader</name>
+    <message>
+        <location filename="../qml/Uploader.qml" line="14"/>
+        <source>Uploader</source>
+        <translation>Uploader</translation>
+    </message>
+    <message>
+        <location filename="../qml/Uploader.qml" line="22"/>
+        <source>Select file</source>
+        <translation>Datei auswählen</translation>
+    </message>
+    <message>
+        <location filename="../qml/Uploader.qml" line="31"/>
+        <source>No file selected</source>
+        <translation>Keine Datei ausgewählt</translation>
+    </message>
+    <message>
+        <location filename="../qml/Uploader.qml" line="36"/>
+        <source>Selected file: %1</source>
+        <translation>Ausgewählte Datei: %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/Uploader.qml" line="45"/>
+        <source>Upload file</source>
+        <translation>Datei hochladen</translation>
+    </message>
+    <message>
+        <location filename="../qml/Uploader.qml" line="56"/>
+        <source>%1  out of %2</source>
+        <extracomment>How much of the file has been downloaded</extracomment>
+        <translation>%1 von %2</translation>
+    </message>
+    <message>
+        <location filename="../qml/Uploader.qml" line="80"/>
+        <source>Cancel</source>
+        <translation>Abbrechen</translation>
+    </message>
+</context>
+</TS>
