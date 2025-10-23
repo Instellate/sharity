0d274a9dc4d576754f5c2bd6e811fb0d9517850b
Instellate <thecrazyrobots@gmail.com>
2025-10-23 09:39:26 +0200

    Started on custom URL scheme support

--- client/cmake/ConfigureCPack.cmake
+++ client/cmake/ConfigureCPack.cmake
@@ -12,8 +12,17 @@ if (WIN32)
         RUNTIME_DEPENDENCY_SET DATACHANNEL_SET
         PRE_EXCLUDE_REGEXES "api-ms-" "ext-ms-" "hvsifiletrust" "C:/msys64/mingw64/bin.*" ".*/msys64/mingw64/bin.*"
         POST_EXCLUDE_REGEXES ".*system32/.*\\.dll" ".*/msys64/mingw64/bin.*"
-        POST_INCLUDE_FILES "C:/windows/system32/libssl-3-x64.dll" "C:/windows/system32/libcrypto-3-x64.dll"
+        POST_INCLUDE_FILES "C:/windows/system32/libssl-3-x64.dll" "C:/windows/system32/libcrypto-3-x64.dll" # There should be a better way than to force include OpenSSL
         LIBRARY DESTINATION bin)
+endif ()
+
+if (CONFIGURE_NSIS)
+    set(CPACK_NSIS_ENABLE_UNINSTALL_BEFORE_INSTALL ON)
+
+    # Files with extra commands
+    # Handles such things as URL scheme registration
+    file(STRINGS "${CMAKE_SOURCE_DIR}/cmake/cpack/nsis/install.nsi" CPACK_NSIS_EXTRA_INSTALL_COMMANDS)
+    file(STRINGS "${CMAKE_SOURCE_DIR}/cmake/cpack/nsis/uninstall.nsi" CPACK_NSIS_EXTRA_UNINSTALL_COMMANDS)
 endif ()
 
 qt_generate_deploy_qml_app_script(
